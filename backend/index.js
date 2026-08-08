1a9c52d32917139ed3ee6fb9fd198580efacdcf0
daniel barbera <135056104+danielbarbera1@users.noreply.github.com>
2026-08-08 18:17:32 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,18 +17,20 @@ app.use(express.json());
 //     database: 'express' // ¡Cambiar por la BD real!
 // };
 
-//PRODUCCION
-const connection = mysql.createConnection({
+// PRODUCCION
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
+  port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
     rejectUnauthorized: false
-  }
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
-
 // 2. Crear una ruta (Endpoint GET) para exponer una API
 
 // POST - Crear nuevo producto
