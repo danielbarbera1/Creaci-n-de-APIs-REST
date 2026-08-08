820b9dd366b1f92747b0e430e75568702e8f0eee
daniel barbera <135056104+danielbarbera1@users.noreply.github.com>
2026-08-08 18:08:13 -0400

    fix
    se le agrego el ssl para ver si con eso se puede hacer el despliegue

--- backend/index.js
+++ backend/index.js
@@ -17,13 +17,17 @@ app.use(express.json());
 //     database: 'express' // ¡Cambiar por la BD real!
 // };
 
+//PRODUCCION
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,// ¡Cambiar por la BD real!
-};
+  database: process.env.DB_NAME
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 // 2. Crear una ruta (Endpoint GET) para exponer una API
 
